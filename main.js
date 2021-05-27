abc49c09a970ca85630ff7cee645087668b03d8a
Arush 6566 <79926945+Arush7735@users.noreply.github.com>
2021-05-27 13:04:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+canvas = document.getElementById("myCanvas");
+
+ctx= canvas.getContext("2d");
+
+
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 4;
+ctx.rect(150, 143, 430, 200, );
+ctx.stroke();
+
+
+   
+    ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+ctx.arc(100, 100, 40, 0, 2 * Math.PI);
+ctx.stroke();
